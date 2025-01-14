c68791353b19fcbfe9c258fc7cc09a93892019b6
wjk <1711980439@qq.com>
2025-01-14 11:29:33 +0800

    feat: test4

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   outDir: 'zmkj/dist',
+  base: '/spadek-w.github.io/', // 这里填github的仓库名称
   title: '智棉科技前端文档',
   description: '前端文档',
   themeConfig: {
